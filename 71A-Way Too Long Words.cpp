6834800e65f825e29d1c6b52786a8b946fe5ca38
Tanjid Khan Arnob <81709574+Tanjid420@users.noreply.github.com>
2022-02-01 22:07:29 +0600

    Add files via upload

---
+++ 71A-Way Too Long Words.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n=0;
+   cin >> n;
+   string myWords;
+    for(int i=1; i<=n ;i++){
+   cin >> myWords;
+   if(myWords.length()>10){
+    cout << myWords[0] <<myWords.length()-2 <<myWords[myWords.length()-1] << endl;
+   }
+   else{
+    cout << myWords << endl;
+   }
+    }
+    return 0;
+
+}
